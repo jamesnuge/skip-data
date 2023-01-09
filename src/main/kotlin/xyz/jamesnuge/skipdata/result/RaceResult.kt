706f4e45e0d83e2d35bd13d1ed0b54c5f3995367
James Nugent <james.nugent89@gmail.com>
2023-01-09 16:51:45 +1100

    Updated result table columns

--- src/main/kotlin/xyz/jamesnuge/skipdata/result/RaceResult.kt
+++ src/main/kotlin/xyz/jamesnuge/skipdata/result/RaceResult.kt
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
 
+// TODO: Updated class to contain new fields
 @Entity
 data class RaceResult(
     @Id
--- src/main/resources/db/migration/V2023_01_06_10_58_11__create_race_result_table.sql
+++ src/main/resources/db/migration/V2023_01_06_10_58_11__create_race_result_table.sql
@@ -1,9 +1,18 @@
 SET ROLE binding_group;
+
 CREATE TABLE race_result(
-    id            BIGSERIAL PRIMARY KEY,
-    datetime      date NOT NULL,
-    result        BIGINT NOT NULL,
-    location      VARCHAR(255) NOT NULL,
-    temperature   BIGINT NOT NULL,
-    humidity      BIGINT NOT NULL
+    id                     BIGSERIAL PRIMARY KEY,
+    datetime               date NOT NULL,
+    location               VARCHAR(255) NOT NULL,
+    temperature            BIGINT NOT NULL,
+    humidity               BIGINT NOT NULL,
+    altitude               BIGINT NOT NULL,
+    track_temperature      BIGINT NOT NULL,
+    trackmeter             BIGINT NOT NULL,
+    sixty_feet_time        BIGINT NOT NULL,
+    three_thirty_feet_time BIGINT NOT NULL,
+    six_sixty_feet_time    BIGINT NOT NULL,
+    six_sixty_feet_speed   BIGINT NOT NULL,
+    quarter_mile_time      BIGINT NOT NULL,
+    quarter_mile_speed     BIGINT NOT NULL
 )
