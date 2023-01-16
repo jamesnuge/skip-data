872fed17714ab5d28c2109c36ecdb4e2b335911a
James Nugent <james.nugent89@gmail.com>
2023-01-17 10:45:44 +1100

    WIP: Started testing

--- src/test/kotlin/xyz/jamesnuge/skipdata/result/RaceResultServerTest.kt
+++ src/test/kotlin/xyz/jamesnuge/skipdata/result/RaceResultServerTest.kt
@@ -14,7 +14,7 @@ import java.time.LocalDate
 import java.time.Instant
 import java.time.ZoneOffset
 
-ExtendWith(MockitoExtension::class)
+@ExtendWith(MockitoExtension::class)
 class RaceResultServiceTest {
 
     companion object {
